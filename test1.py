c8778f7727fd6ebd568f126ed771170495395428
saanviGarg01 <158058912+saanviGarg01@users.noreply.github.com>
2024-05-30 16:41:45 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,10 @@
+a = "33"
+b = "33"
+
+if b > a:
+    print("b is greater than a")
+
+elif a == b:
+    print("b is equal to a")
+
+ 
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_Function(fname):
+    print(fname+ "Refsnes")
+
+my_Function("emil")
+my_Function("tobias")
+my_Function("linus")
